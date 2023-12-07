3f7b3822c4a91f37ceb629d781ed028f7f6e6c3b
Jin Park <103798287+jinftw64@users.noreply.github.com>
2023-12-07 14:45:38 -0500

    Updates

---
+++ precache-manifest.fc7eb56f49ff22c01e0ba6c3af1acc89.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a929ae6a4d9d36f400cae89e495b8468",
+    "url": "/u-dicom-viewer/index.html"
+  },
+  {
+    "revision": "725a385b16a408bcc356",
+    "url": "/u-dicom-viewer/static/css/2.18fb2ca0.chunk.css"
+  },
+  {
+    "revision": "ea848f48c1e85f815b9d",
+    "url": "/u-dicom-viewer/static/css/main.fa5b3062.chunk.css"
+  },
+  {
+    "revision": "725a385b16a408bcc356",
+    "url": "/u-dicom-viewer/static/js/2.041284f0.chunk.js"
+  },
+  {
+    "revision": "6ad6b4e8816ec58fe488c5d7be2cf06a",
+    "url": "/u-dicom-viewer/static/js/2.041284f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea848f48c1e85f815b9d",
+    "url": "/u-dicom-viewer/static/js/main.6c212f46.chunk.js"
+  },
+  {
+    "revision": "4e9954b4f9bc47440a7d",
+    "url": "/u-dicom-viewer/static/js/runtime-main.1153c50b.js"
+  }
+]);
